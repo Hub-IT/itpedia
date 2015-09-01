2a36911acf7dd9cd0a78b9410d8bab6d018ac8b1
Rizart Dokollari <r.dokollari@gmail.com>
2015-09-01 18:47:07 +0300

    Add ability to add 'active' class for active route/action

---
+++ DB/Utilities/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+/**
+ * Credits: http://www.codingeverything.com/2014/05/mvcbootstrapactivenavbar.html
+ * @author Rizart Dokollari <r.dokollari@gmail.com>
+ * @since 9/1/2015
+ */
+namespace DB.Utilities
+{
+    public static class Extensions
+    {
+        public static string IsActive(this HtmlHelper html, string control, string action)
+        {
+            var routeData = html.ViewContext.RouteData;
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+
+            var returnActive = control == routeControl && action == routeAction;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
